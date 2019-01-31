61cbebe3b92073fee718d21264762a9fd167e0ad
Cecilie Moriat <ceciliemoriat@gmail.com>
2019-01-31 14:03:50 +0100

    some code

---
+++ assignment0/Assets/Collide.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collide : MonoBehaviour
+{
+    GameObject Earth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Earth = GameObject.Find("Earth");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Earth.transform.position)
+        {
+            
+        }
+    }
+}
