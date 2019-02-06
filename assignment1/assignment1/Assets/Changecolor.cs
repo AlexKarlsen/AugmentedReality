654f625c7853f12cfb820b5ffd759953e106ded1
Alex Karlsen <alexkarlsen7@gmail.com>
2019-02-06 15:29:32 +0100

    C almost done!

---
+++ assignment1/assignment1/Assets/Changecolor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changecolor : MonoBehaviour
+{
+    GameObject SpaceShuttle;
+    GameObject LandingLane;
+    Color Red = new Color(255, 0, 0);
+    Color Green = new Color(0, 255, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpaceShuttle = GameObject.Find("SpaceShuttle");
+        LandingLane = GameObject.Find("LandingLane");
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (LandingLane != null && SpaceShuttle != null)
+        {
+            print("Spaceshuttle coords.: " + SpaceShuttle.transform.position.ToString());
+            print("Landing Lane coords.: " + LandingLane.transform.position.ToString());
+            print("Dot Product = " + Vector3.Dot(SpaceShuttle.transform.position, LandingLane.transform.position).ToString());
+
+            if (Vector3.Dot(SpaceShuttle.transform.position, LandingLane.transform.position) > 0.5)
+            {
+                print("Landing");
+                SpaceShuttle.GetComponent<Renderer>().material.color = Green;
+            }
+            else
+            {
+                SpaceShuttle.GetComponent<Renderer>().material.color = Red;
+            }
+        }
+    }
+}
---
+++ assignment1/assignment1/Assets/landing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class landing : MonoBehaviour
+{
+
+    GameObject SpaceShuttle;
+    GameObject LandingLane;
+    GameObject Quad;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpaceShuttle = GameObject.Find("SpaceShuttle");
+        LandingLane = GameObject.Find("LandingLane");
+        Quad = GameObject.Find("SpaceShuttleQuad");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(SpaceShuttle.ToString());
+        print(LandingLane.ToString());
+
+        if(Vector3.Dot(SpaceShuttle.transform.position, LandingLane.transform.position).Equals(0))
+        { 
+        }
+    }
+}
